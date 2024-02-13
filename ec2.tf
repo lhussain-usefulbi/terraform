e3348456138232d84b7c3d2d1facce0f51b63537
lhussain-usefulbi <118711474+lhussain-usefulbi@users.noreply.github.com>
2024-02-13 16:57:27 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -15,3 +15,12 @@ resource "aws_instance" "foo-2" {
     Name = "tf-example-2"
   }
 }
+
+resource "aws_instance" "foo-3" {
+  ami           = "ami-0a7cf821b91bcccbc" 
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "tf-example-3"
+  }
+}
